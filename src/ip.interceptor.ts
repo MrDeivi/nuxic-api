ed02f6bcfbf8dd0bd69b5047e240744fb1fafa15
MrDeivi <dalfonsogcia@gmail.com>
2024-05-01 16:51:01 -0400

    fix: rate limit
    Signed-off-by: MrDeivi <dalfonsogcia@gmail.com>

--- src/ip.interceptor.ts
+++ src/ip.interceptor.ts
@@ -1,27 +1,35 @@
-import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common'
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common'
+import { Cache } from 'cache-manager'
 import * as dotenv from 'dotenv'
 import { Request } from 'express'
 import { Observable } from 'rxjs'
 
 dotenv.config()
 
+const REQUEST_LIMIT = parseInt(`${process.env.REQUEST_LIMIT ?? 50}`)
+
 @Injectable()
 export class BlacklistedIpInterceptor implements NestInterceptor {
-  private readonly blacklistedIps: string[]
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  constructor() {
-    this.blacklistedIps = process.env.BLACKLISTED_IPS.split(',')
-  }
-
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
     const request: Request = context.switchToHttp().getRequest()
     const header = request.get('x-forwarded-for')
     const clientIp = header ?? request.ip
 
-    const isBlocked = this.blacklistedIps.includes(clientIp)
-    console.log(`ip: ${clientIp}  isBlocked: ${isBlocked}`)
+    const { success, count } = await rateLimitRequest(clientIp, this.cacheManager)
+    console.log(`ip: ${clientIp}  isBlocked: ${!success} count: ${count}`)
 
-    if (isBlocked) {
+    if (!success) {
       throw new HttpException(
         { statusCode: HttpStatus.TOO_MANY_REQUESTS, error: 'Too Many Requests', message: 'Rate limit exceeded.' },
         429,
@@ -30,4 +38,33 @@ export class BlacklistedIpInterceptor implements NestInterceptor {
 
     return next.handle()
   }
+}
+
+export async function rateLimitRequest(ip: string, cacheManager) {
+  if (!ip) return { success: false }
+
+  const { success, count } = await limit(ip, cacheManager)
+
+  console.log(`isBlocked: ${!success} ip: ${ip} count: ${count}`)
+
+  const error = success
+    ? null
+    : new Response('You have reached your request limit.', {
+        status: 429,
+      })
+
+  return { success, error, count }
+}
+
+const blackList: string[] = []
+
+async function limit(ip: string | undefined, cacheManager: Cache): Promise<{ success: boolean; count?: number }> {
+  if (!ip) return { success: false }
+
+  const requests: number = (await cacheManager.get(ip)) ?? 0
+  if (requests > REQUEST_LIMIT) blackList.push(ip)
+
+  cacheManager.set(ip, requests + 1)
+
+  return { success: !blackList.includes(ip), count: requests }
 }
